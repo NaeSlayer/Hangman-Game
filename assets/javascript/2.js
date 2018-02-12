4cfb7622a0477f15f1b05966dc1881c9c7dc93f5
lanae <rlfoard@gmail.com>
2018-02-11 20:39:20 -0700

    more progress

--- assets/javascript/2.js
+++
@@ -1,5 +0,0 @@
-var words = ['pizza', 'javascript', 'netflix'];
-
-var computerWord = //code to get random word from array;
-	
-var computerLetters = computerWord.split('');
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,15 +6,27 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 12;
 var alreadyGuessed = [];
-var incorrectLetters = [];
-var winningLetters = [];
+// var incorrectLetters = [];
+// var winningLetters = [];
 var currentWord = "";
 var validLetters = [];
 var answerArray = [];
 var wordSplit = [];
+var wordBlanks = [];
+
 
 //Functions
 function updateWins() {
+    document.getElementById("wins").innerHTML = wins;
+    
+}
+ function checkForWin(){
+    for (var i =0; i < wordSplit.length; i++) {
+    if (wordBlanks.indexOf("_") === -1) {
+        setupWord();
+        wins++;
+    }
+}
     document.getElementById("wins").innerHTML = wins;
 }
 function updateLosses() {
@@ -24,20 +36,17 @@ function updateGuessesLeft() {
     document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
     if (guessesLeft < 1) {
+        guessesLeft = 12;
+        
         losses++;
-        resetGlobalVariables();
+        setupWord();
+        updateGuessesLeft();
+        
 }
 }
 
-//function to store words in winning letter array
-//var uniqurArr = arr.filter(function(item, pos, self) {
-  //  return(self.indexOf(item)===-1)
-//});
 
 
-updateWins();
-//updateLosses();
-updateGuessesLeft();
 
 
 
@@ -47,65 +56,115 @@ document.onkeyup = function (event) {
     // Determines which key was pressed.
     var userGuess = event.key;
     userGuess = userGuess.toLowerCase();
-    userGuess.textContent = event.key;
-    console.log(userGuess);
-    console.log(letters.indexOf(userGuess));
-  
     
-    function updateValidLetters() {
-        console.log(letters.indexOf(userGuess));
-        if (letters.indexOf(userGuess) != -1 && validLetters.indexOf(userGuess) ===-1) {
-            // validLetters.push(userGuess);
-            document.getElementById("validLetters").innerHTML = validLetters.push(userGuess);
-            document.getElementById("validLetters").innerHTML = validLetters;
+    
+    if (wordSplit.indexOf(userGuess) >= 0) {
+        // userGuess is correct.
+        // var letterIndex = wordSplit.indexOf(userGuess);
+        var letterIndices = getLetterIndices();
+
+        for (var i = 0; i < letterIndices.length; i++) {
+            var letterIndex = letterIndices[i];
+            wordBlanks[letterIndex] = userGuess;
+            letterIndex;
+        }
+
+        
+
+        printWord();
+    } else {
+        // userGuess is incorrect.
+        // Decrent guesess.
+        // guessesLeft--;
+    }
+        function getLetterIndices() {
+            var letterIndices = [];
+            for (var i = 0; i < wordSplit.length; i++) {
+                if (wordSplit[i] === userGuess) {
+                    letterIndices.push(i);
+                }
+            }
+
+            return letterIndices;
             
+        }
+    
+    userGuess.textContent = event.key;
+    console.log("userguess: " + userGuess);
+    console.log("index of letter guessed " + letters.indexOf(userGuess));
+  
+    function updateValidLetters() {
+        // console.log(letters.indexOf(userGuess));
+        if (letters.indexOf(userGuess) != -1 && validLetters.indexOf(userGuess) === -1) {
+            console.log("index of letter guessed " + letters.indexOf(userGuess));
+            validLetters.push(userGuess);
+            // document.getElementById("validLetters").innerHTML = validLetters.push(userGuess);
+            document.getElementById("validLetters").innerHTML = validLetters;
+            // guessesLeft--;
+            
+
         }
 
     }
 
     function updateAlreadyGuessed() {
-        if (letters.indexOf(userGuess) != -1) {
-            document.getElementById("alreadyGuessed").innerHTML = alreadyGuessed.push(userGuess);
+        if (letters.indexOf(userGuess) != -1 && alreadyGuessed.indexOf(userGuess) === -1) {
+            guessesLeft--;
+            console.log("index of letter guessed " + letters.indexOf(userGuess));
+            alreadyGuessed.push(userGuess);
+            // document.getElementById("alreadyGuessed").innerHTML = alreadyGuessed.push(userGuess);
             document.getElementById("alreadyGuessed").innerHTML = alreadyGuessed;
+            
         }
+    };
+  
+    updateGuessesLeft();
+    console.log("guessesLeft " + guessesLeft);
+    //  updateValidLetters(); 
+     updateAlreadyGuessed();
+    updateWins();
+    updateLosses();
+    // checkForWin();
     }
-    updateValidLetters(); 
-    updateAlreadyGuessed();
-}
 
+
+    
+    
+    
+    
+
+function setupWord() {
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     var currentWord = words[Math.floor(Math.random() * words.length)];
+    wordBlanks = [];
+    alreadyGuessed = [];
     console.log("current word: " + currentWord);
-
-
     wordSplit = currentWord.split("");
     console.log("word split " + wordSplit);
-    // document.getElementById("currentWord").innerHTML = currentWord.push(validLetter[1]);
-    // document.getElementById("currentWord").innerHTML = currentWord;
 
-    for (var i = 0; i < words.length; i++) {
-        currentWord[i] = "_";
-        
+
+    for (var i = 0; i < wordSplit.length; i++) {
+        wordBlanks[i] = "_";
     }
+   printWord();
+   
+      
+}
+function printWord(){
+    document.getElementById("currentWord").innerHTML = wordBlanks.join(" ");
+   
 
-        //return words.split('');
-        //document.getElementById("currentWord").innerHTML = currentWord.push(words);
-        //document.getElementById("currentWord").innerHTML = currentWord;
+}
+updateWins();
+updateLosses();
+updateGuessesLeft();
+setupWord();
+
 
        
             
-    //}
-    function splitString(words, comma) {
-        var arrayOfStrings = words[i].split(comma);
-
-        console.log('The original string is: "' + words + '"');
-        console.log('The separator is: "' + comma + '"');
-        console.log('The array has ' + arrayOfStrings.length + ' elements: ' + arrayOfStrings.join(' / '));
-
-       
-    }
-//splitString();
-
+   
+   
 
 
 
